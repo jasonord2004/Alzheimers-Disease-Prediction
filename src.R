f7081eb3c91cd263e3ab93384cf846204fa58724
jasonord <jason.ord2004@gmail.com>
2025-04-19 16:34:57 -0400

    Paraphrase
    Summarized Course Goals

--- src.R
+++ src.R
@@ -1,1 +1,13 @@
 # Fraud Detection Project
+
+data <- read.csv(".csv")
+
+#1. Describe data through Visuals (Charts/Tables/Graphs, etc)
+
+#2. Classification Procedures
+  # Rpart Model
+    # Show Confusion Matrix from Decision Tree
+  # Naive Bayes Model
+    # Use classification variable and predictors chosen from rpart
+    # Show Confusion Matrix
+  # Compare accuraies
